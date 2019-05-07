172a1fc3e404967518e4fe51855770d34c84572d
elboso <31boso@gmail.com>
2019-05-07 12:34:11 +0200

    Add buton events

--- code.js
+++ code.js
@@ -19,3 +19,30 @@ smolTitle.style.color = 'blue';
 smolTitle.textContent = 'I’m a blue h3!'
 
 container.appendChild(smolTitle);
+
+// crée une boite rose et ses deux contenu
+const blackBox = document.createElement('div');
+blackBox.classList.add('blackBox');
+
+const boxTitle = document.createElement('h1');
+boxTitle.textContent = "I\'m a div";
+blackBox.appendChild(boxTitle);
+
+const boxText = document.createElement('p');
+boxText.textContent = 'ME TOO!'
+blackBox.appendChild(boxText);
+
+container.appendChild(blackBox);
+
+// Bouton version 2
+const btn = document.querySelector('#btn');
+btn.onclick = alerte;
+
+// Bouton version 3
+const button2 = document.querySelector('#button2');
+button2.addEventListener('click', alerte);
+
+// function d'alerte Bouton
+function alerte () {
+  alert('fudge!')
+};
